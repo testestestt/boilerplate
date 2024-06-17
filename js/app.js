420690c80bbeea07d6f8fcbc714b7c07233696c4
Robe Jafet <99152729+RobeJafet@users.noreply.github.com>
2024-06-16 19:03:32 -0600

    Data toggle show in app.js

--- js/app.js
+++ js/app.js
@@ -2,3 +2,10 @@ $(document).ready(function () {
   $("header").load("../partials/header.html");
   $("footer").load("../partials/footer.html");
 });
+
+
+$(document).on('click', '[data-toggle="show"]', function (event) {
+  event.preventDefault()
+  var target = $(this).data('target')
+  $(target).toggleClass('show')
+})
